714cf81ea4a86d6b925812f5fbf056629b850023
rishitkamboj <rishit242@gmail.com>
2024-03-28 22:23:00 +0530

    changed jwt type to text rather than json

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,7 +3,7 @@ import { cors } from 'hono/cors'
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 import {sign,verify} from 'hono/jwt';
-import {signupInput} from '@rishitkamboj/common-zod';
+import {signupInput,signinInput} from '@rishitkamboj/common-zod';
 
 
 export const userRouter = new Hono<{
@@ -37,12 +37,14 @@ userRouter.post('/signup', async (c) => {
      });
      const jwt=await sign({id:u.id},c.env.JWT_SECRET);
    
-   return c.json({jwt});
+   return c.text(jwt);
    
    }
-     catch(e){
-       return c.status(403);
-     }
+   catch(e) {
+    console.log(e);
+    c.status(411);
+    return c.text('Invalid')
+  }
    
    });
    
@@ -51,9 +53,14 @@ userRouter.post('/signup', async (c) => {
      const prisma = new PrismaClient({
        datasourceUrl: c.env.DATABASE_URL,
    }).$extends(withAccelerate())
+   const body = await c.req.json();
    
-     const body = await c.req.json();
-   
+   const {success}=signinInput.safeParse(body);
+   if(!success){
+    c.status(411);
+    return c.json({message:"Invalid Input"});
+   }
+   try{
        const u=await prisma.user.findUnique({
      where:{ email:body.email}
      });
@@ -64,7 +71,12 @@ userRouter.post('/signup', async (c) => {
      }
    
      const jwt=await sign({id:u.id},c.env.JWT_SECRET);
-     return  c.json({jwt});
+     return c.text(jwt);}
+     catch(e) {
+      console.log(e);
+      c.status(411);
+      return c.text('Invalid')
+    }
     });
    
    
