5a8b8e7114bc681d1d4c88e20a2ed7280439f3ee
rishitkamboj <rishit242@gmail.com>
2024-03-29 13:53:12 +0530

    added fonts for headings

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,3 +1,12 @@
+import { Avatar } from "./Blogcard"
+
 export const Appbar=()=>{
-     
+     return <div className="border-b flex justify-between px-10 py-4">
+<div className="flex flex-col justify-center rubik-scribble-regular">SpeakUp</div>
+
+<div>
+     <Avatar name="Rishit" size="big"/>
+</div>
+
+     </div>
 }
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,35 +1,21 @@
+import { Appbar } from "../components/Appbar"
 import { Blogcard } from "../components/Blogcard"
 
 export const Blogs=()=>{
-     return <div className="flex justify-center">
+     return <div >
+          <Appbar/>
+          <div className="flex justify-center">
           <div className=" max-xl">
           <Blogcard authorName={"Rishit"}
 title={"IDK"}
 content={"This is a blog"}
 publishedDate={"2021-09-01T00:00:00.000Z"}
     /> 
-              <Blogcard authorName={"Rishit"}
-title={"IDK"}
-content={"This is a blog"}
-publishedDate={"2021-09-01T00:00:00.000Z"}
-    /> 
-              <Blogcard authorName={"Rishit"}
-title={"IDK"}
-content={"This is a blog"}
-publishedDate={"2021-09-01T00:00:00.000Z"}
-    /> 
-    
-    
-    
-    
-    
-    
-    
-    
+              
     
     </div>
 
 
-    
+    </div>
     </div>
 }
