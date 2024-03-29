1b435797606e956fdd3928e2da3edf1975cdabc2
rishitkamboj <rishit242@gmail.com>
2024-03-29 21:48:18 +0530

    done

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -8,7 +8,7 @@ export const Appbar=()=>{
 </Link>
 <div> <Link to={"/publish"}>
 <button type="button" className=" mr-6 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">New</button>
-</Link>  <Avatar name="Rishit" size="big"/>
+</Link>  <Avatar name="Rishit" size="big" />
 </div>
 
      </div>
